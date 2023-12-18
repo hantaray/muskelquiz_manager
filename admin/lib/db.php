9d5510abddb7ba4de2bfac7f9250f8fa40e65132
hansashi <ahantke@mail.de>
2023-12-18 19:09:53 +0100

    refactoring

--- admin/lib/db.php
+++ admin/lib/db.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $dsn = "mysql:host=127.0.0.1;dbname=muskelabcd_muskelDB";
 $user = "muskelabcd_physioUnited";
 $pass = "muskelabcd_physioUnited";
@@ -6,14 +6,13 @@ $options = array(
 	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 	PDO::ATTR_PERSISTENT => true,
 	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-   );
-try{
-//Instanz von PDO (Datenbankverbindung aufbauen)
-$db = new PDO($dsn, $user, $pass, $options);
-//UTF8 für Querys
-$db->query("SET NAMES utf8");
-}
-catch(PDOException $err){
-echo "Ein Fehler ist aufgetreten!";
-echo $err->getMessage();
+);
+try {
+	//Instanz von PDO (Datenbankverbindung aufbauen)
+	$db = new PDO($dsn, $user, $pass, $options);
+	//UTF8 für Querys
+	$db->query("SET NAMES utf8");
+} catch (PDOException $err) {
+	echo "Ein Fehler ist aufgetreten!";
+	echo $err->getMessage();
 }
